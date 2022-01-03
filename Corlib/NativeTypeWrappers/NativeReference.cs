69d7219edb4fd709536ff29f644dbde8b73cab97
nifanfa <nifanfa@foxmail.com>
2022-01-03 12:35:52 +0800

    Import from RoseOS

---
+++ Corlib/NativeTypeWrappers/NativeReference.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+using Internal.Runtime.CompilerServices;
+
+namespace NativeTypeWrappers {
+	[StructLayout(LayoutKind.Sequential)]
+	public readonly struct ReadonlyNativeReference<T> where T : unmanaged {
+		readonly IntPtr _pointer;
+
+		/// <summary>Gets the object pointed to by the native pointer as a reference.</summary>
+		public ref T Ref => ref Unsafe.AsRef<T>(_pointer);
+	}
+}
