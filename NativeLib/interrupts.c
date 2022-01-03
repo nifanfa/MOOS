48aac018dec588bb21984410b12ec58ed1b9a6fb
nifanfa <nifanfa@foxmail.com>
2022-01-03 13:25:18 +0800

    Update Corlib

--- NativeLib/interrupts.c
+++ NativeLib/interrupts.c
@@ -14,15 +14,15 @@ typedef struct {
 
 
 int
-isr0(), isr1(), isr2(), isr3(), isr4(), isr5(), isr6(), isr7(),
-isr8(), isr9(), isr10(), isr11(), isr12(), isr13(), isr14(), isr15(),
-isr16(), isr17(), isr18(), isr19(), isr20(), isr21(), isr22(), isr23(),
-isr24(), isr25(), isr26(), isr27(), isr28(), isr29(), isr30(), isr31(),
-isr128();
+	isr0(), isr1(), isr2(), isr3(), isr4(), isr5(), isr6(), isr7(),
+	isr8(), isr9(), isr10(), isr11(), isr12(), isr13(), isr14(), isr15(),
+	isr16(), isr17(), isr18(), isr19(), isr20(), isr21(), isr22(), isr23(),
+	isr24(), isr25(), isr26(), isr27(), isr28(), isr29(), isr30(), isr31(),
+	isr128();
 
 int
-irq0(), irq1(), irq2(), irq3(), irq4(), irq5(), irq6(), irq7(),
-irq8(), irq9(), irq10(), irq11(), irq12(), irq13(), irq14(), irq15();
+	irq0(), irq1(), irq2(), irq3(), irq4(), irq5(), irq6(), irq7(),
+	irq8(), irq9(), irq10(), irq11(), irq12(), irq13(), irq14(), irq15();
 
 
 void load_gdt(void* gdtr) {
