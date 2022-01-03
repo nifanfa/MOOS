9c408ec72bdaa6d387c3bcd4d2cc97c2469c3a8d
nifanfa <nifanfa@foxmail.com>
2022-01-03 13:48:56 +0800

    Import from RoseOS

--- Kernel/Program.cs
+++ Kernel/Program.cs
@@ -5,18 +5,21 @@ unsafe class Program
     [RuntimeExport("Main")]
     static void Main()
     {
-        for (uint i = 0; i < 0x40000; i ++)
+        //You don't need to build. Just save and run Launcher
+        Console.Setup();
+        Console.Write('o');
+
+        for (uint i = 512; i < 0x2000; i ++)
         {
-            Allocator.AddFreePages((System.IntPtr)((1024*1024)+(i*4096)), i);
+            Allocator.AddFreePages((System.IntPtr)(i*4096), i-512);
         }
+
+        Console.Write('k');
 
         IDT.Disable();
         //GDT.Initialise();
         IDT.Initialise();
         IDT.Enable();
-
-        //You don't need to build. Just save and run Launcher
-        Console.Setup();
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Use Native AOT (Core RT) Technology.");
         for (; ; );
