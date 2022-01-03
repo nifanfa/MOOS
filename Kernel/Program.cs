b180c025d961d9b18c3a034d4bf5526296e29d5d
nifanfa <nifanfa@foxmail.com>
2022-01-03 14:02:59 +0800

    Update Program.cs

--- Kernel/Program.cs
+++ Kernel/Program.cs
@@ -7,7 +7,6 @@ unsafe class Program
     {
         //You don't need to build. Just save and run Launcher
         Console.Setup();
-        Console.Write('o');
 
         //                 10MiB                 512MiB                1MiB
         for (uint i = 1024 * 1024 * 10; i < 1024 * 1024 * 512; i += 1024 * 1024)
@@ -15,8 +14,6 @@ unsafe class Program
             //                                      1MiB / 4KiB
             Allocator.AddFreePages((System.IntPtr)(i), 256);
         }
-
-        Console.Write('k');
 
         IDT.Disable();
         //GDT.Initialise();
