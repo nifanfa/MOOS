1beb89e9f83f61400943fcaf035af7a9fed03998
nifanfa <nifanfa@foxmail.com>
2022-01-22 19:30:01 +0800

    Rename

--- NativeLib/io.c
+++ NativeLib/io.c
@@ -1,16 +1,16 @@
 
-void outb(unsigned short port, unsigned char value) {
+void Out8(unsigned short port, unsigned char value) {
 	__outbyte(port, value);
 }
 
-void outw(unsigned short port, unsigned short value) {
+void Out16(unsigned short port, unsigned short value) {
 	__outword(port, value);
 }
 
-unsigned char inb(unsigned short port) {
+unsigned char In8(unsigned short port) {
 	return __inbyte(port);
 }
 
-unsigned short inw(unsigned short port) {
+unsigned short In16(unsigned short port) {
 	return __inword(port);
 }
