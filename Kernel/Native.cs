e976a72c201c2bd1c0b9eb29cb1d3e21e0f0ef84
nifanfa <nifanfa@foxmail.com>
2022-01-22 17:50:58 +0800

    Update

--- Kernel/Native.cs
+++ Kernel/Native.cs
@@ -1,19 +1,42 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
-public static class Native
+
+static class Native
 {
     [DllImport("*")]
     public static extern void outb(ushort port, byte value);
 
     [DllImport("*")]
+    public static extern void outw(ushort port, ushort value);
+
+    [DllImport("*")]
     public static extern byte inb(ushort port);
+
+    [DllImport("*")]
+    public static extern IntPtr kmalloc(ulong size);
+
+    [DllImport("*")]
+    public static extern IntPtr krealloc(IntPtr ptr, ulong newSize);
+
+    [DllImport("*")]
+    public static extern IntPtr kcalloc(ushort num, ushort size);
+
+    [DllImport("*")]
+    public static extern void kfree(IntPtr ptr);
+
+    [DllImport("*")]
+    public static extern void load_gdt(ref GDT.GDTDescriptor gdtr);
 
     [DllImport("*")]
     public static extern void load_idt(ref IDT.IDTDescriptor idtr);
 
     [DllImport("*")]
-    public static extern void _sti();
+    public static extern void _hlt();
 
     [DllImport("*")]
     public static extern void _cli();
+
+    [DllImport("*")]
+    public static extern void _sti();
 }
