a7976d4f85e179cebf7274e8a00fd15307926731
nifanfa <nifanfa@foxmail.com>
2022-01-23 03:28:50 +0800

    Update Network

---
+++ Corlib/System/Collections/Generic/List.cs
@@ -0,0 +1,28 @@
+﻿namespace System.Collections.Generic
+{
+    public class List<T>
+    {
+        private T[] value;
+
+        public ulong Count = 0;
+
+        public List(ulong initsize)
+        {
+            value = new T[initsize];
+        }
+
+        public T this[ulong index]
+        {
+            get
+            {
+                return value[index];
+            }
+        }
+
+        public void Add(T t)
+        {
+            value[Count] = t;
+            Count++;
+        }
+    }
+}
---
+++ Kernel/NET/Network.cs
@@ -0,0 +1,9 @@
+﻿namespace Kernel.NET
+{
+    public static class Network
+    {
+        public static byte[] MAC;
+        public static byte[] IP;
+        public static byte[] Boardcast = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+    }
+}
