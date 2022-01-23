2c26e106bdda1644bbed381176bacd75d1578312
nifanfa <nifanfa@foxmail.com>
2022-01-23 23:48:58 +0800

    Update IDT

--- Kernel/Misc/IDT.cs
+++ Kernel/Misc/IDT.cs
@@ -106,122 +106,30 @@ public static class IDT
         }
     }
 
-    [RuntimeExport("irq0_handler")]
-    public static void IRQ0Handler()
+    [RuntimeExport("irq_handler")]
+    public static void IRQHandler(int irq) 
     {
-        PIT.OnInterrupt();
-        PIC.EndOfInterrupt(0x20);
-    }
-
-    [RuntimeExport("irq1_handler")]
-    public static void IRQ1Handler()
-    {
-        byte b = Native.In8(0x60);
-        char c = PS2Keyboard.ProcessKey(b);
-        if (c != '?') Console.Write(c);
-        PIC.EndOfInterrupt(0x21);
-    }
-
-    [RuntimeExport("irq2_handler")]
-    public static void IRQ2Handler()
-    {
-        Console.WriteLine("IRQ 2");
-        PIC.EndOfInterrupt(0x22);
-    }
-
-    [RuntimeExport("irq3_handler")]
-    public static void IRQ3Handler()
-    {
-        Console.WriteLine("IRQ 3");
-        PIC.EndOfInterrupt(0x23);
-    }
-
-    [RuntimeExport("irq4_handler")]
-    public static void IRQ4Handler()
-    {
-        Console.WriteLine("IRQ 4");
-        PIC.EndOfInterrupt(0x24);
-    }
-
-    [RuntimeExport("irq5_handler")]
-    public static void IRQ5Handler()
-    {
-        Console.WriteLine("IRQ 5");
-        PIC.EndOfInterrupt(0x25);
-    }
-
-    [RuntimeExport("irq6_handler")]
-    public static void IRQ6Handler()
-    {
-        Console.WriteLine("IRQ 6");
-        PIC.EndOfInterrupt(0x26);
-    }
-
-    [RuntimeExport("irq7_handler")]
-    public static void IRQ7Handler()
-    {
-        Console.WriteLine("IRQ 7");
-        PIC.EndOfInterrupt(0x27);
-    }
-
-    [RuntimeExport("irq8_handler")]
-    public static void IRQ8Handler()
-    {
-        Console.WriteLine("IRQ 8");
-        PIC.EndOfInterrupt(0x28);
-    }
-
-    [RuntimeExport("irq9_handler")]
-    public static void IRQ9Handler()
-    {
-        Console.WriteLine("IRQ 9");
-        PIC.EndOfInterrupt(0x29);
-    }
-
-    [RuntimeExport("irq10_handler")]
-    public static void IRQ10Handler()
-    {
-        Console.WriteLine("IRQ 10");
-        PIC.EndOfInterrupt(0x2A);
-    }
-
-    [RuntimeExport("irq11_handler")]
-    public static void IRQ11Handler()
-    {
-        //Console.WriteLine("IRQ 11");
-        if(RTL8139.IRQ == 0x2B)
+        irq += 0x20;
+        switch (irq)
         {
-            RTL8139.OnInterrupt();
+            case 0x20:
+                PIT.OnInterrupt();
+                break;
+            case 0x21:
+                byte b = Native.In8(0x60);
+                char c = PS2Keyboard.ProcessKey(b);
+                if (c != '?') Console.Write(c);
+                break;
+            case 0x2B:
+                if (RTL8139.IRQ == 0x2B)
+                {
+                    RTL8139.OnInterrupt();
+                }
+                break;
+            case 0x2C:
+                PS2Mouse.OnInterrupt();
+                break;
         }
-
-        PIC.EndOfInterrupt(0x2B);
-    }
-
-    [RuntimeExport("irq12_handler")]
-    public static void IRQ12Handler()
-    {
-        PS2Mouse.OnInterrupt();
-        PIC.EndOfInterrupt(0x2C);
-    }
-
-    [RuntimeExport("irq13_handler")]
-    public static void IRQ13Handler()
-    {
-        Console.WriteLine("IRQ 13");
-        PIC.EndOfInterrupt(0x2D);
-    }
-
-    [RuntimeExport("irq14_handler")]
-    public static void IRQ14Handler()
-    {
-        Console.WriteLine("IRQ 14");
-        PIC.EndOfInterrupt(0x2E);
-    }
-
-    [RuntimeExport("irq15_handler")]
-    public static void IRQ15Handler()
-    {
-        Console.WriteLine("IRQ 15");
-        PIC.EndOfInterrupt(0x2F);
+        PIC.EndOfInterrupt(irq);
     }
 }
