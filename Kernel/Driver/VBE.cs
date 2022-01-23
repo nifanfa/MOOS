dcd91531c1b6f131162ca4d06ff69a7884466f8f
nifanfa <nifanfa@foxmail.com>
2022-01-24 02:26:00 +0800

    Update VBE.cs

--- Kernel/Driver/VBE.cs
+++ Kernel/Driver/VBE.cs
@@ -7,12 +7,12 @@
         public static void Initialise(VBEInfo* info)
         {
             Info = info;
-            if(info->PhysBase != 0) 
+            ASC16.Initialise();
+            if (info->PhysBase != 0) 
             {
                 Framebuffer.VideoMemory = (uint*)info->PhysBase;
                 Framebuffer.SetVideoMode(info->ScreenWidth, info->ScreenHeight);
                 Framebuffer.Clear(0x0);
-                ASC16.Initialise();
             }
         }
     }
