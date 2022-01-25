3d494160f64615f08f1e2559ea0dd1d024d5a0c5
nifanfa <nifanfa@foxmail.com>
2022-01-25 10:05:02 +0800

    Revert "Update SMBIOS.cs"
    This reverts commit 054bb0638d7071e81acab05308ceabd28e0c6e34.

--- Kernel/Driver/SMBIOS.cs
+++ Kernel/Driver/SMBIOS.cs
@@ -98,14 +98,9 @@ namespace Kernel.Driver
 
         public static void Initialise()
         {
-            int p = 0xF0000;
-            while (*(uint*)p != 0x5F4D535F && p < 0xFFFFF) p++;
-            if(p>= 0xFFFFF) 
-            {
-                Console.WriteLine("[SMBIOS]: Entry not found!");
-                return;
-            }
-            Console.WriteLine("[SMBIOS]: Entry found!");
+            byte* p = (byte*)0xF0000;
+            while (*(uint*)p != 0x5F4D535F) p++;
+            Console.WriteLine("SMBIOS Entry Found");
 
             SMBIOSEntryPoint* entry = (SMBIOSEntryPoint*)p;
             NewMethod(entry);
