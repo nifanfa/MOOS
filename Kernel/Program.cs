7c5c5dd5de10889115c570a9fc84ca30a266f621
nifanfa <nifanfa@foxmail.com>
2022-01-26 16:47:15 +0800

    Update ILCompiler version

--- Kernel/Program.cs
+++ Kernel/Program.cs
@@ -45,7 +45,7 @@ unsafe class Program
         PIT.Initialise();
         PS2Mouse.Initialise();
         ACPI.Initialize();
-        SMBIOS.Initialise();
+        //SMBIOS.Initialise();
 
         Serial.WriteLine("Hello World");
         Console.WriteLine("Hello, World!");
