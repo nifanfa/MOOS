372e7a21f11cc947f4e6f69b6d9ce9ee1d73737a
nifanfa <nifanfa@foxmail.com>
2022-01-29 14:12:13 +0800

    Update In/Outsb/w/d

--- NativeLib/native.cpp
+++ NativeLib/native.cpp
@@ -5,6 +5,36 @@ void Stosb(void* p, unsigned char value, unsigned long count)
 	__stosb(p, value, count);
 }
 
+void Insd(unsigned short Port,unsigned int* Data,unsigned long Count)
+{
+	__indwordstring(Port, Data, Count);
+}
+
+void Outsd(unsigned short Port,unsigned int* Data,unsigned long Count)
+{
+	__outdwordstring(Port, Data, Count);
+}
+
+void Insw(unsigned short Port, unsigned short* Data, unsigned long Count)
+{
+	__inwordstring(Port, Data, Count);
+}
+
+void Outsw(unsigned short Port, unsigned short* Data, unsigned long Count)
+{
+	__outwordstring(Port, Data, Count);
+}
+
+void Insb(unsigned short Port, unsigned char* Data, unsigned long Count)
+{
+	__inbytestring(Port, Data, Count);
+}
+
+void Outsb(unsigned short Port, unsigned char* Data, unsigned long Count)
+{
+	__outbytestring(Port, Data, Count);
+}
+
 void Stosd(void* p, unsigned int value, unsigned long count)
 {
 	__stosd(p, value, count);
