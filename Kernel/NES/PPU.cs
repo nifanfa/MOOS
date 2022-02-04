806a400e2dc861fc170f84c0124d73baa69d5a47
nifanfa <nifanfa@foxmail.com>
2022-02-04 20:06:16 +0800

    Bug fix for NES emulator

--- Kernel/NES/PPU.cs
+++ Kernel/NES/PPU.cs
@@ -546,14 +546,14 @@ namespace NES
         {
             // Update all previously retreived sprite values as the new current values
             spritesFound = spritesFoundT;           // Stores the location of each sprite found
+            spriteToDraw.Dispose();
             spriteToDraw = spriteToDrawT;           // Number of sprites found
             spriteIndex = spriteIndexT;             // Any sprites found?
             memory.memCPU[0x2002] &= memCPU2002T;   // Reset '8 sprites on a scanline' bit
 
             // Reset Temp Sprite Values for next evaluation
-            for (int i = 0; i < spriteToDrawT.Length; i++) spriteToDrawT[i] = 0;
-             //spriteToDrawT = new byte[8];    // Stores the location of each sprite found
-             spriteIndexT = 0x00;            // Number of sprites found
+            spriteToDrawT = new byte[8];    // Stores the location of each sprite found
+            spriteIndexT = 0x00;            // Number of sprites found
             spritesFoundT = false;          // Any sprites found?
             memCPU2002T = 0xDF;             // Reset '8 sprites on a scanline' bit
 
