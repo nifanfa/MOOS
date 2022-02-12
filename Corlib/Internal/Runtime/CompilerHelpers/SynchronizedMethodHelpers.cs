c65cb5dd6ceea7bb0a0157bbebc4a6cad5a8731a
nifanfa <nifanfa@foxmail.com>
2022-02-13 00:03:46 +0800

    Event Implementation

---
+++ Corlib/Internal/Runtime/CompilerHelpers/SynchronizedMethodHelpers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Internal.Runtime.CompilerHelpers
+{
+    internal static class SynchronizedMethodHelpers
+    {
+        private static void MonitorEnterStatic(IntPtr pEEType, ref bool lockTaken) { lockTaken = true; }
+
+        private static void MonitorExitStatic(IntPtr pEEType, ref bool lockTaken) { lockTaken = false; }
+    }
+}
