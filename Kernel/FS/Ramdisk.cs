4a2f49ec3de8bfc46bdb9572d98a483b91e3f39d
nifanfa <nifanfa@foxmail.com>
2022-02-14 10:01:48 +0800

    Create Ramdisk.cs

---
+++ Kernel/FS/Ramdisk.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Kernel.FS
+{
+    public unsafe class Ramdisk : Disk
+    {
+        byte* ptr;
+
+        public Ramdisk(IntPtr _ptr)
+        {
+            ptr = (byte*)_ptr;
+        }
+
+        public override bool Read(ulong sector, uint count, byte[] data) 
+        {
+            fixed(byte* p = data) 
+            {
+                Native.Movsb(p, ptr + (sector * 512), 512 * count);
+            }
+            return true;
+        }
+
+        public override bool Write(ulong sector, uint count, byte[] data)
+        {
+            fixed (byte* p = data)
+            {
+                Native.Movsb(ptr + (sector * 512), p, 512 * count);
+            }
+            return true;
+        }
+    }
+}
