f85d0db51d37b3a6eedb971edf2fe40c0927f35a
nifanfa <nifanfa@foxmail.com>
2022-02-14 10:26:10 +0800

    Update Framebuffer.cs

--- Kernel/Driver/Framebuffer.cs
+++ Kernel/Driver/Framebuffer.cs
@@ -114,8 +114,32 @@ namespace Kernel
         public static void DrawImage(int X, int Y, Image image)
         {
             for (int h = 0; h < image.Height; h++)
-                for (int w = 0; w < image.Width; w++)
-                    DrawPoint(X + w, Y + h, image.RawData[image.Width * h + w]);
+                for (int w = 0; w < image.Width; w++) 
+                {
+                    uint foreground = image.RawData[image.Width * h + w];
+                    int fA = (byte)((foreground >> 24) & 0xFF);
+                    int fR = (byte)((foreground >> 16) & 0xFF);
+                    int fG = (byte)((foreground >> 8) & 0xFF);
+                    int fB = (byte)((foreground) & 0xFF);
+
+                    uint background = GetPoint(X + w, Y + h);
+                    int bA = (byte)((background >> 24) & 0xFF);
+                    int bR = (byte)((background >> 16) & 0xFF);
+                    int bG = (byte)((background >> 8) & 0xFF);
+                    int bB = (byte)((background) & 0xFF);
+
+                    int alpha = fA;
+                    int inv_alpha = 255 - alpha;
+
+                    int newR = (fR * alpha + inv_alpha * bR) >> 8;
+                    int newG = (fG * alpha + inv_alpha * bG) >> 8;
+                    int newB = (fB * alpha + inv_alpha * bB) >> 8;
+
+                    if (fA != 0)
+                    {
+                        DrawPoint(X + w, Y + h, Color.ToArgb((byte)newR, (byte)newG, (byte)newB));
+                    }
+                }
         }
     }
 }
