822471c62b43e71267e6f11f742ce7f4553bf786
nifanfa <nifanfa@foxmail.com>
2022-02-15 06:45:22 +0800

    Update Form.cs

--- Kernel/GUI/Form.cs
+++ Kernel/GUI/Form.cs
@@ -64,7 +64,7 @@ namespace Kernel.GUI
 
             Framebuffer.Fill(X, Y - BarHeight - 5, Width, 5, 0xFF1B5DAA);
             Framebuffer.Fill(X, Y - BarHeight, Width, BarHeight, 0xFF3C3C3C);
-            ASC16.DrawString(Title, X + (BarHeight/4), Y - (BarHeight / 2) - (16 / 2), 0xFFFFFFFF);
+            ASC16.DrawString(Title, X + (BarHeight/4), Y - BarHeight + (BarHeight / 4), 0xFFFFFFFF);
             Framebuffer.Fill(X, Y, Width, Height, BackgroundColor);
         }
     }
