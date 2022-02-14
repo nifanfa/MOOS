4c4d774226dfbd0e722132a1d621012c6fb7edfa
nifanfa <nifanfa@foxmail.com>
2022-02-15 04:41:48 +0800

    Add CapsLock

--- Corlib/System/Types.cs
+++ Corlib/System/Types.cs
@@ -36,6 +36,14 @@ namespace System
             return r;
         }
 
+        public char ToUpper() 
+        {
+            char chr = this;
+            if (chr >= 'a' && chr <= 'z')
+                chr -= (char)('a' - 'A');
+            return chr;
+        }
+
         public static bool IsDigit(char c)
             => c >= '0' && c <= '9';
     }
