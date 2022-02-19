3218fa157c90787bbd6056f2519279195df2e7d8
nifanfa <nifanfa@foxmail.com>
2022-02-19 23:54:24 +0800

    Update GC

---
+++ Corlib/System/ObsoleteAttribute.cs
@@ -0,0 +1,34 @@
+﻿namespace System
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum |
+        AttributeTargets.Interface | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Delegate,
+        Inherited = false)]
+    public sealed class ObsoleteAttribute : Attribute
+    {
+        public ObsoleteAttribute()
+        {
+            Message = null;
+            IsError = false;
+        }
+
+        public ObsoleteAttribute(string? message)
+        {
+            Message = message;
+            IsError = false;
+        }
+
+        public ObsoleteAttribute(string? message, bool error)
+        {
+            Message = message;
+            IsError = error;
+        }
+
+        public string? Message { get; }
+
+        public bool IsError { get; }
+
+        public string? DiagnosticId { get; set; }
+
+        public string? UrlFormat { get; set; }
+    }
+}
