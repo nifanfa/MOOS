65b4c5cef8be6447f44a788012e863afa25fe2c7
nifanfa <nifanfa@foxmail.com>
2022-03-30 23:56:35 +0800

    Update TCP

--- src/OS-Sharp/Networking/TCP.cs
+++ src/OS-Sharp/Networking/TCP.cs
@@ -31,7 +31,19 @@ namespace OS_Sharp.Networking
         public uint rcvNxt;                        // receive next
         public uint rcvWnd;                        // receive window
         public uint rcvUP;                         // receive urgent pointer
-        public uint irs;                            // initial receive sequence number
+
+        public void Send(byte[] buffer) 
+        {
+            if (Connected) 
+            {
+                fixed (byte* p = buffer)
+                    TCP.Send(this, p, buffer.Length);
+            }
+            else
+            {
+                Console.WriteLine("Connection havn't established yet");
+            }
+        }
     }
 
     public enum TCPStatus
@@ -72,17 +84,7 @@ namespace OS_Sharp.Networking
     internal unsafe class TCP
     {
         [StructLayout(LayoutKind.Sequential, Pack = 1)]
-        public struct NetBuf
-        {
-            public byte* start;          // offset to data start
-            public byte* end;            // offset to data end exclusive
-            public uint refCount;
-            public uint seq;            // Data from TCP header used for out-of-order/retransmit
-            public byte flags;          // Data from TCP header used for out-of-order/retransmit
-        }
-
-        [StructLayout(LayoutKind.Sequential, Pack = 1)]
-        public struct TCPHeader
+        private struct TCPHeader
         {
             public ushort srcPort;
             public ushort dstPort;
@@ -109,23 +111,7 @@ namespace OS_Sharp.Networking
             public ushort len;
         }
 
-        private static ulong s_baseIsn = 0;
-
-        public static void Init()
-        {
-            ulong t =
-                (ulong)(RTC.Second +
-        RTC.Minute * 60 +
-        RTC.Hour * 3600 +
-        RTC.Day * 86400 +
-        (RTC.Year - 70) * 31536000 +
-        ((RTC.Year - 69) / 4) * 86400 -
-        ((RTC.Year - 1) / 100) * 86400 +
-        ((RTC.Year + 299) / 400) * 86400);
-            s_baseIsn = (t * 1000 - PIT.Tick) * 250;
-        }
-
-        internal static void TcpRecv(byte* buffer, int length)
+        internal static void HandlePacket(byte* buffer, int length)
         {
             TCPHeader* hdr = (TCPHeader*)buffer;
             buffer += hdr->off >> 2;
@@ -310,6 +296,8 @@ namespace OS_Sharp.Networking
                 case TCPStatus.FinWait2:
                 case TCPStatus.CloseWait:
                 case TCPStatus.Closing:
+                    //Accept ack
+                    if (hdr->seq == PacketAck) PacketSent = true;
                     // Handle expected acks
                     if ((conn.sndUna - hdr->ack) <= 0 && (hdr->ack - conn.sndNxt) <= 0)
                     {
@@ -442,7 +430,6 @@ namespace OS_Sharp.Networking
             {
                 // SYN is set.  ACK is either ok or there was no ACK.  No RST.
 
-                conn.irs = hdr->seq;
                 conn.rcvNxt = hdr->seq + 1;
 
                 if ((flags & (byte)TCPFlags.TCP_ACK) != 0)
@@ -503,20 +490,17 @@ namespace OS_Sharp.Networking
             conn.localPort = localPort;
             conn.remotePort = port;
 
-            ulong isn = s_baseIsn + PIT.Tick * 250;
-
-            conn.sndUna = (uint)isn;
-            conn.sndNxt = (uint)isn;
+            conn.sndUna = 0;
+            conn.sndNxt = 0;
             conn.sndWnd = TCP_WINDOW_SIZE;
             conn.sndUP = 0;
             conn.sndWl1 = 0;
             conn.sndWl2 = 0;
-            conn.iss = (uint)isn;
+            conn.iss = 0;
 
             conn.rcvNxt = 0;
             conn.rcvWnd = TCP_WINDOW_SIZE;
             conn.rcvUP = 0;
-            conn.irs = 0;
 
             // Issue SYN segment
             SendPacket(conn, conn.sndNxt, (byte)TCPFlags.TCP_SYN);
@@ -536,17 +520,17 @@ namespace OS_Sharp.Networking
             return conn;
         }
 
-        public static void SendPacket(TCPConnection conn, uint seq, byte flags)
+        private static void SendPacket(TCPConnection conn, uint seq, byte flags)
         {
             SendPacket(conn, conn.sndNxt, flags, null, 0);
         }
 
-        public static void SendPacket(TCPConnection conn, uint seq, byte flags, void* data, uint count)
+        private static void SendPacket(TCPConnection conn, uint seq, byte flags, void* data, uint count)
         {
-            NetBuf* pkt = NetAllocBuf();
+            byte * buffer = (byte*)Allocator.Allocate(TCP_WINDOW_SIZE);
 
             // Header
-            TCPHeader* hdr = (TCPHeader*)pkt->start;
+            TCPHeader* hdr = (TCPHeader*)buffer;
             hdr->srcPort = conn.localPort;
 
             hdr->dstPort = conn.remotePort;
@@ -559,7 +543,7 @@ namespace OS_Sharp.Networking
             hdr->urgent = 0;
             Swap(hdr);
 
-            byte* p = pkt->start + sizeof(TCPHeader);
+            byte* p = buffer + sizeof(TCPHeader);
 
             if ((flags & (byte)TCPFlags.TCP_SYN) != 0)
             {
@@ -571,12 +555,12 @@ namespace OS_Sharp.Networking
             }
 
             // Option End
-            while (((p - pkt->start) & 3) != 0)
+            while (((p - buffer) & 3) != 0)
             {
                 *p++ = 0;
             }
 
-            hdr->off = (byte)((p - pkt->start) << 2);
+            hdr->off = (byte)((p - buffer) << 2);
 
             // Data
             if (data != null)
@@ -584,10 +568,10 @@ namespace OS_Sharp.Networking
                 Native.Movsb(p, data, count);
             }
 
-            pkt->end = p + count;
+            byte* end = p + count;
 
             // Pseudo Header
-            ChecksumHeader* phdr = (ChecksumHeader*)(pkt->start - sizeof(ChecksumHeader));
+            ChecksumHeader* phdr = (ChecksumHeader*)(buffer - sizeof(ChecksumHeader));
             phdr->src[0] = conn.localAddr[0];
             phdr->src[1] = conn.localAddr[1];
             phdr->src[2] = conn.localAddr[2];
@@ -599,11 +583,11 @@ namespace OS_Sharp.Networking
 
             phdr->reserved = 0;
             phdr->protocol = (byte)IPv4.IPv4Protocol.TCP;
-            phdr->len = Ethernet.SwapLeftRight((ushort)((uint)pkt->end - (uint)pkt->start));
+            phdr->len = Ethernet.SwapLeftRight((ushort)((uint)end - (uint)buffer));
 
             // Checksum
             //ushort checksum = NetChecksum(pkt->start - sizeof(ChecksumHeader), pkt->end);
-            ushort checksum = TCPChecksum(pkt->start - sizeof(ChecksumHeader), pkt->end);
+            ushort checksum = TCPChecksum(buffer - sizeof(ChecksumHeader), end);
             hdr->checksum = Ethernet.SwapLeftRight(checksum);
 
             // Transmit
@@ -613,7 +597,9 @@ namespace OS_Sharp.Networking
                 conn.remoteAddr [1],
                 conn.remoteAddr [2],
                 conn.remoteAddr [3]
-            }, (byte)IPv4.IPv4Protocol.TCP, pkt->start, (int)pkt->end - (int)pkt->start);
+            }, (byte)IPv4.IPv4Protocol.TCP, buffer, (int)end - (int)buffer);
+
+            Allocator.Free((System.IntPtr)buffer);
 
             // Update State
             conn.sndNxt += count;
@@ -621,14 +607,6 @@ namespace OS_Sharp.Networking
             {
                 ++conn.sndNxt;
             }
-        }
-
-        private static NetBuf* NetAllocBuf()
-        {
-            NetBuf* buf = (NetBuf*)Allocator.Allocate(2048);
-            buf->start = (byte*)buf + 256;
-            buf->end = (byte*)buf + 256;
-            return buf;
         }
 
         private static void Swap(TCPHeader* hdr)
@@ -644,19 +622,28 @@ namespace OS_Sharp.Networking
             hdr->urgent = Ethernet.SwapLeftRight(hdr->urgent);
         }
 
+        private static bool PacketSent = false;
+        private static uint PacketAck = 0;
+
         public static void Send(TCPConnection conn, byte* data, int count)
         {
             if (conn.Connected)
             {
-                SendPacket(conn, conn.sndNxt, (byte)TCPFlags.TCP_ACK | (byte)TCPFlags.TCP_PSH, data, (uint)count);
-            }
-            else
-            {
-                Console.WriteLine("Connection havn't established yet");
+                PacketSent = false;
+                PacketAck = conn.rcvNxt;
+                uint sndNxt = conn.sndNxt;
+                for (; ; )
+                {
+                    SendPacket(conn, sndNxt, (byte)TCPFlags.TCP_ACK | (byte)TCPFlags.TCP_PSH, data, (uint)count);
+                    PIT.Wait(1000);
+                    if (PacketSent) break;
+                    Console.WriteLine("Packet may not accpeted. resending");
+                }
+                Console.WriteLine("Packet sent");
             }
         }
 
-        public static ushort TCPChecksum(byte* data, byte* end)
+        private static ushort TCPChecksum(byte* data, byte* end)
         {
             uint len = (uint)(end - data);
             ushort* p = (ushort*)data;
