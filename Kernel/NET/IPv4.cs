d5abf92d87a64063a589f34895ded5ba824a3569
nifanfa <nifanfa@foxmail.com>
2022-04-03 13:24:29 +0800

    Renew

--- Kernel/NET/IPv4.cs
+++ Kernel/NET/IPv4.cs
@@ -6,7 +6,7 @@ namespace Kernel.NET
 {
     public static unsafe class IPv4
     {
-        [StructLayout(LayoutKind.Sequential,Pack = 1)]
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
         public unsafe struct IPv4Header
         {
             public byte VersionAndIHL;
@@ -34,12 +34,12 @@ namespace Kernel.NET
             data += sizeof(IPv4Header);
             length -= sizeof(IPv4Header);
 
-            if(
+            if (
                 hdr->DestIP[0] == Network.IP[0] &&
                 hdr->DestIP[1] == Network.IP[1] &&
                 hdr->DestIP[2] == Network.IP[2] &&
                 hdr->DestIP[3] == Network.IP[3]
-                ) 
+                )
             {
                 if (hdr->Protocol == (byte)IPv4Protocol.ICMP)
                 {
@@ -73,6 +73,16 @@ namespace Kernel.NET
             }
         }
 
+        public static bool IsSameSubnet(byte[] ip1, byte[] ip2)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if ((ip1[i] & Network.Mask[i]) != (ip2[i] & Network.Mask[i])) return false;
+            }
+
+            return true;
+        }
+
         public static void SendPacket(byte[] DestIP, byte Protocol, byte* Data, int Length)
         {
             IPv4Header* hdr = (IPv4Header*)Allocator.Allocate((ulong)(sizeof(IPv4Header) + Length));
@@ -84,13 +94,14 @@ namespace Kernel.NET
             hdr->SourceIP[1] = Network.IP[1];
             hdr->SourceIP[2] = Network.IP[2];
             hdr->SourceIP[3] = Network.IP[3];
+
             hdr->DestIP[0] = DestIP[0];
             hdr->DestIP[1] = DestIP[1];
             hdr->DestIP[2] = DestIP[2];
             hdr->DestIP[3] = DestIP[3];
             hdr->HeaderChecksum = CalculateChecksum((byte*)hdr, sizeof(IPv4Header));
             Native.Movsb(((byte*)hdr) + sizeof(IPv4Header), Data, (ulong)Length);
-            byte[] MAC = ARP.Lookup(DestIP);
+            byte[] MAC = ARP.Lookup(IsSameSubnet(DestIP, Network.IP) ? DestIP : Network.Gateway);
             Ethernet.SendPacket(MAC, (ushort)Ethernet.EthernetType.IPv4, hdr, sizeof(IPv4Header) + Length);
             Allocator.Free((IntPtr)hdr);
         }
@@ -107,10 +118,14 @@ namespace Kernel.NET
             }
 
             if (count > 0)
+            {
                 sum += *(byte*)ptr;
+            }
 
             while ((sum >> 16) != 0)
+            {
                 sum = (sum & 0xffff) + (sum >> 16);
+            }
 
             return (ushort)~sum;
         }
