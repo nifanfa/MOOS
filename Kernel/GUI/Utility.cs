8afa3fba076dea13f397bcc8bdf810e703d38fe4
nifanfa <nifanfa@foxmail.com>
2022-04-03 19:45:40 +0800

    Update Utility

---
+++ Kernel/GUI/Utility.cs
@@ -0,0 +1,15 @@
+﻿namespace Kernel.GUI
+{
+    internal class Utility : Form
+    {
+        public Utility(int X, int Y) : base(X, Y, 300, 200)
+        {
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            Framebuffer.Fill(X, Y, Width, Height, 0xFFFF0000);
+        }
+    }
+}
