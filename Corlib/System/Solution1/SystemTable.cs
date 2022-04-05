317a25d0ed7e9b3daba8b305a5f3167081f09493
nifanfa <nifanfa@foxmail.com>
2022-04-05 16:31:59 +0800

    Update API

---
+++ Corlib/System/Solution1/SystemTable.cs
@@ -0,0 +1,7 @@
+﻿namespace System.Solution1
+{
+    public unsafe struct SystemTable
+    {
+        public delegate*<string, void> Console_WriteLine;
+    }
+}
---
+++ Kernel/API.cs
@@ -0,0 +1,15 @@
+﻿using System.Solution1;
+
+namespace Kernel
+{
+    public static unsafe class API
+    {
+        public static SystemTable st;
+
+        public static void Initialize() 
+        {
+            st = new SystemTable();
+            st.Console_WriteLine = &Console.WriteLine;
+        }
+    }
+}
