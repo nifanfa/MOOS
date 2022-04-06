29ea367bff66fdf469dfd5f43aa8276d733307e7
nifanfa <nifanfa@foxmail.com>
2022-04-07 00:05:46 +0800

    Add API

--- Kernel/API.cs
+++ Kernel/API.cs
@@ -9,17 +9,33 @@ namespace Kernel
         public static unsafe void HandleSystemCall(IDTStack* stack)
         {
             var pCell = (MethodFixupCell*)stack->rcx;
-            string name = string.FromASCII(pCell->MethodName, strings.strlen((byte*)pCell->MethodName));
+            string name = string.FromASCII(pCell->Module->ModuleName, strings.strlen((byte*)pCell->Module->ModuleName));
             switch (name)
             {
                 case "SayHello":
                     stack->rax = (ulong)(delegate*<void>)&SayHello;
+                    break;
+                case "Console.WriteLine":
+                    stack->rax = (ulong)(delegate*<char*, void>)&API_WriteLine;
                     break;
                 default:
                     Panic.Error($"System call \"{name}\" is not found");
                     break;
             }
             name.Dispose();
+        }
+
+        //String will become char* if we use DllImport
+        public static void API_WriteLine(char* c) 
+        {
+            int i = 0;
+            while (c[i] != 0)
+            {
+                Console.Write(c[i]);
+                i++;
+            }
+
+            Console.WriteLine();
         }
 
         public static void SayHello()
