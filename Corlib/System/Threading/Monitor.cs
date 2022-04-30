c1c8721b8947f2a486bf39ebbb662b48ac9dcc88
nifanfa <nifanfa@foxmail.com>
2022-04-30 19:19:46 +0800

    Add lock support

---
+++ Corlib/System/Threading/Monitor.cs
@@ -0,0 +1,20 @@
+﻿#if Kernel
+using Kernel;
+using Kernel.Misc;
+
+namespace System.Threading
+{
+    public static class Monitor
+    {
+        public static void Enter(object obj)
+        {
+            ThreadPool.Locked = true;
+        }
+
+        public static void Exit(object obj)
+        {
+            ThreadPool.Locked = false;
+        }
+    }
+}
+#endif
