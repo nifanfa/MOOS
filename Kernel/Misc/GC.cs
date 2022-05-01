31d900c58cdf7086656e77df1ab7167f2af45720
nifanfa <nifanfa@foxmail.com>
2022-05-01 14:18:37 +0800

    Update GC

--- Kernel/Misc/GC.cs
+++ Kernel/Misc/GC.cs
@@ -6,48 +6,58 @@ namespace Kernel.Misc
 {
     public unsafe static class GC
     {
-        public static sbyte CollectIf;
+        [Flags]
+        public enum Flags : byte 
+        {
+            Fixed = 0b1000_0000,
+            NeedsToBeCollected = 0b0100_0000,
+        }
+
         public static bool AllowCollect;
-        public static sbyte NotCollectIf;
 
         public static void Collect()
         {
-            ulong addr;
-            ulong memSaved = 0;
-            ulong counter = 0;
-            for (ulong i = 0; i < Allocator.NumPages; i++)
+            lock (null)
             {
-                if (Allocator._Info.Pages[i] == 0) continue;
-                if (Allocator._Info.Pages[i] == Allocator.PageSignature) continue;
-                if (Allocator._Info.GCInfos[i] == NotCollectIf) continue;
-
-                addr = (ulong)(Allocator._Info.Start + (i * Allocator.PageSize));
-                ulong* page = PageTable.GetPage(addr);
-                if (BitHelpers.IsBitSet(*page, 5)) //Accessed bit
+                ulong addr;
+                ulong memSaved = 0;
+                ulong counter = 0;
+                for (ulong i = 0; i < Allocator.NumPages; i++)
                 {
-                    *page &= ~(1UL << 5);
-                    Allocator._Info.GCInfos[i]++;
-                }
-                else
-                {
-                    Allocator._Info.GCInfos[i]--;
-                }
+                    if (Allocator._Info.Pages[i] == 0) continue;
+                    if (Allocator._Info.Pages[i] == Allocator.PageSignature) continue;
+                    if (((Flags)Allocator._Info.GCInfos[i]).HasFlag(Flags.Fixed)) continue;
 
-                if (Allocator._Info.GCInfos[i] < CollectIf)
-                {
-                    counter++;
-                    memSaved += Allocator.Free((IntPtr)addr);
-                }
+                    addr = (ulong)(Allocator._Info.Start + (i * Allocator.PageSize));
+                    ulong* page = PageTable.GetPage(addr);
+                    if (BitHelpers.IsBitSet(*page, 5)) //Accessed bit
+                    {
+                        *page &= ~(1UL << 5);
+                        Allocator._Info.GCInfos[i] = 0;
+                    }
+                    else
+                    {
+                        Allocator._Info.GCInfos[i]++;
+                    }
 
-                i += Allocator._Info.Pages[i];
-            }
-            if (memSaved != 0)
-            {
-                Debug.Write("GC Collected: ");
-                Debug.Write(counter.ToString());
-                Debug.Write(" Unused Handle(s) ");
-                Debug.Write((memSaved / 1048576).ToString());
-                Debug.WriteLine("MiB");
+                    ulong pages = Allocator._Info.Pages[i];
+
+                    if (((Flags)Allocator._Info.GCInfos[i]).HasFlag(Flags.NeedsToBeCollected))
+                    {
+                        counter++;
+                        memSaved += Allocator.Free((IntPtr)addr);
+                    }
+
+                    i += pages;
+                }
+                if (memSaved != 0)
+                {
+                    Console.Write("GC Collected: ");
+                    Console.Write(counter.ToString());
+                    Console.Write(" Unused Handle(s) ");
+                    Console.Write((memSaved / 1048576).ToString());
+                    Console.WriteLine("MiB");
+                }
             }
         }
     }
