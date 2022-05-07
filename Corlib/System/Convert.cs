389d6ce0449c5eceb2c8bd37669026f894bbc2e2
nifanfa <nifanfa@foxmail.com>
2022-05-07 21:39:01 +0800

    Update Convert.cs

--- Corlib/System/Convert.cs
+++ Corlib/System/Convert.cs
@@ -44,5 +44,58 @@ namespace System
         {
             return b;
         }
+
+        public static long ToInt64(string str)
+        {
+            int i = 0;
+            long val = 0;
+            bool neg = false;
+            if (str[0] == '-')
+            {
+                i = 1;
+                neg = true;
+            }
+            for (; i < str.Length; i++)
+            {
+                val *= 10;
+                val += str[i] - 0x30;
+            }
+            return neg ? -(val) : val;
+        }
+
+        public static int ToInt32(string str)
+        {
+            return (int)ToInt64(str);
+        }
+
+        public static short ToInt16(string str)
+        {
+            return (short)ToInt64(str);
+        }
+
+        public static sbyte ToInt8(string str)
+        {
+            return (sbyte)ToInt64(str);
+        }
+
+        public static ulong ToUInt64(string str)
+        {
+            return (ulong)ToInt64(str);
+        }
+
+        public static uint ToUInt32(string str)
+        {
+            return (uint)ToInt64(str);
+        }
+
+        public static ushort ToUInt16(string str)
+        {
+            return (ushort)ToInt64(str);
+        }
+
+        public static byte ToUInt8(string str)
+        {
+            return (byte)ToInt64(str);
+        }
     }
 }
