01c3363b5ff52e5343557ef5d79a3097ae44863b
nifanfa <nifanfa@foxmail.com>
2022-05-08 12:54:59 +0800

    Update SSE and AVX

---
+++ Kernel/AVX.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Kernel
+{
+    public static class AVX
+    {
+        [DllImport("*")]
+        public static extern void enable_avx();
+    }
+}
---
+++ Kernel/SSE.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Kernel
+{
+    internal static class SSE
+    {
+        [DllImport("*")]
+        public static extern void enable_sse();
+    }
+}
