c40e0ece7c0acd05ab60349d50d9158aed799366
nifanfa <nifanfa@foxmail.com>
2022-05-11 13:07:04 +0800

    Create MP.cs

---
+++ Kernel/MP.cs
@@ -0,0 +1,27 @@
+﻿namespace Kernel
+{
+    public static unsafe class MP
+    {
+        public struct MPPointer
+        {
+            public uint Signature;
+            public uint MPConfig;
+            public byte Length;
+            public byte Version;
+            public byte Checksum;
+            public byte Feature1;
+            public uint Feature2_3;
+        }
+
+        public static void Initialize()
+        {
+            uint* ptr = (uint*)0xF0000;
+            while(*ptr != 0x5f504d5f && ptr < (uint*)0xfffff)
+            {
+                ptr++;
+            }
+            Console.WriteLine($"ptr:{((uint)ptr).ToString("x2")}");
+            Console.ReadKey();
+        }
+    }
+}
