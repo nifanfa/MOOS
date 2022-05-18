b28525084c5674f37b187c5b997fff20931cb6b3
nifanfa <nifanfa@foxmail.com>
2022-05-19 01:00:37 +0800

    Update HPET.cs

--- Kernel/Driver/HPET.cs
+++ Kernel/Driver/HPET.cs
@@ -22,25 +22,35 @@ namespace Kernel.Driver
             }
 
             //1 Femtosecond= 1e-15 sec
-            Clock = (In64((ulong*)(ACPI.HPET->Addresses.Address + 0)) >> 32);
-            Out64((ulong*)(ACPI.HPET->Addresses.Address + 0x10), 0);
-            Out64((ulong*)(ACPI.HPET->Addresses.Address + 0xF0), 0);
-            Out64((ulong*)(ACPI.HPET->Addresses.Address + 0x10), 1);
+            Clock = (In(0) >> 32);
+            Out(0x10, 0);
+            Out(0xF0, 0);
+            Out(0x10, 1);
             Console.WriteLine("[HPET] HPET Initialized");
+        }
+
+        public static void Out(ulong reg,ulong value)
+        {
+            Out64((ulong*)(ACPI.HPET->Addresses.Address + reg), value);
+        }
+
+        public static ulong In(ulong reg)
+        {
+            return In64((ulong*)(ACPI.HPET->Addresses.Address + reg));
         }
 
         public static void ConfigureTimer(int Index,ulong Millionseconds, ulong IRQ)
         {
             ulong time = (Millionseconds * 10000000000000) / Clock;
-            Out64((ulong*)(ACPI.HPET->Addresses.Address + (ulong)(0x100 + 0x20 * Index)), (IRQ << 9) | (1 << 2) | (1 << 3) | (1 << 6));
-            Out64((ulong*)(ACPI.HPET->Addresses.Address + (ulong)(0x108 + 0x20 * Index)), In64((ulong*)(ACPI.HPET->Addresses.Address + 0)) + time);
-            Out64((ulong*)(ACPI.HPET->Addresses.Address + (ulong)(0x108 + 0x20 * Index)), time);
+            Out((ulong)(0x100 + 0x20 * Index), (IRQ << 9) | (1 << 2) | (1 << 3) | (1 << 6));
+            Out((ulong)(0x108 + 0x20 * Index), In(0) + time);
+            Out((ulong)(0x108 + 0x20 * Index), time);
             Interrupts.EnableInterrupt((byte)IRQ);
         }
 
         public static ulong GetTickCount()
         {
-            return In64((ulong*)(ACPI.HPET->Addresses.Address + 0xF0));
+            return In(0xF0);
         }
 
         public static void Wait(ulong Millionseconds) 
