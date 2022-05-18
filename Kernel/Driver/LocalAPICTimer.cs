25fd7c46774eb7e9c4982d6b746961022e28b991
nifanfa <nifanfa@foxmail.com>
2022-05-18 23:37:57 +0800

    Update LAPICTimer

---
+++ Kernel/Driver/LocalAPICTimer.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Copyright(c) 2022 nifanfa, This code is part of the Moos licensed under the MIT licence.
+ */
+namespace Kernel.Driver
+{
+    public static partial class LocalAPIC
+    {
+        public static ulong Ticks => In(0x390);
+
+        public static void StartTimer(ulong freq, uint vector)
+        {
+            Out(0x320, 0x00020000 | vector);
+            Out(0x3e0, 0x3);
+            Out(0x380, (uint)(freq));
+        }
+
+        public static void StopTimer()
+        {
+            Out(0x320, 0x00020000 | 0x10000);
+        }
+    }
+}
