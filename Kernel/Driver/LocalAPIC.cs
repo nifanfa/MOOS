70733fa83a2faf404f7c185158aec40402531f96
nifanfa <nifanfa@foxmail.com>
2022-06-08 23:39:14 +0800

    Update LocalAPIC.cs

--- Kernel/Driver/LocalAPIC.cs
+++ Kernel/Driver/LocalAPIC.cs
@@ -80,8 +80,8 @@ namespace MOOS.Driver
             Out((uint)LAPIC_TPR, 0);
 
             // Logical Destination Mode
-            //Out((uint)LAPIC_DFR, 0xffffffff);   // Flat mode
-            //Out((uint)LAPIC_LDR, 0 << 24);   // All cpus use logical id 0
+            Out((uint)LAPIC_DFR, 0xffffffff);   // Flat mode
+            Out((uint)LAPIC_LDR, 0 << 24);   // All cpus use logical id 0
 
             // Configure Spurious Interrupt Vector Register
             Out((uint)LAPIC_SVR, 0x100 | 0xff);
