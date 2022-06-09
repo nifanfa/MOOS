985a8fe45bcce966e7c39eab2d78b51e008301e2
nifanfa <nifanfa@foxmail.com>
2022-06-09 16:00:45 +0800

    Revert "Update Panic"
    This reverts commit a831c69d0b4bf8f4e11edbdae929b010b0d0feca.

--- Kernel/Misc/Panic.cs
+++ Kernel/Misc/Panic.cs
@@ -7,15 +7,10 @@ namespace MOOS.Misc
 {
     public static class Panic
     {
-        public static int CPUHalted;
-
         public static void Error(string msg,bool skippable = false)
         {
             //Kill all CPUs
-            Panic.CPUHalted = 0;
             LocalAPIC.SendAllInterrupt(0xFD);
-            while (Panic.CPUHalted != (SMP.NumCPU - 1)) Native._pause();
-
             IDT.Disable();
             Framebuffer.TripleBuffered = false;
             Console.Write("PANIC: ");
