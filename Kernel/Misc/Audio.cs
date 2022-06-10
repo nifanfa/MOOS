859b9fe20e470fc6b822e9470472a715a406bd06
nifanfa <nifanfa@foxmail.com>
2022-06-10 15:15:46 +0800

    Update Audio

---
+++ Kernel/Misc/Audio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MOOS.Misc
+{
+    public static unsafe class Audio
+    {
+        public const int SampleRate = 48000;
+
+        public static Queue<byte[]> Queue;
+
+        public static void Initialize() 
+        {
+            Queue = new();
+        }
+
+        public static void Play(byte[] pcm) 
+        {
+            fixed(byte* ptr_pcm = pcm)
+            {
+                for (int i = pcm.Length; i >= 0; i -= SampleRate * 2)
+                {
+                    byte[] pack = new byte[SampleRate * 2];
+                    fixed (byte* ptr = pack)
+                    {
+                        Native.Movsb(ptr, ptr_pcm + i, (ulong)pack.Length);
+                    }
+                    Queue.Enqueue(pack);
+                }
+            }
+        }
+    }
+}
