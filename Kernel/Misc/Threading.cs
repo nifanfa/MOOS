89095d1d010c511114931377c7f1fbc172c6bcd0
nifanfa <nifanfa@foxmail.com>
2022-06-13 05:42:58 +0800

    Update Threading.cs

--- Kernel/Misc/Threading.cs
+++ Kernel/Misc/Threading.cs
@@ -101,7 +101,6 @@ namespace MOOS.Misc
 
         public static void Initialize()
         {
-            Native.Cli();
             //Bootstrap CPU
             if(SMP.ThisCPU == 0)
             {
@@ -122,7 +121,6 @@ namespace MOOS.Misc
             {
                 new Thread(&IdleThread).Start((int)SMP.ThisCPU, true);
             }
-            Native.Sti();
             _int20h(); //start scheduling
         }
 
