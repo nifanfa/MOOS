f5d2e6dd74a349f5bef329b1a298327393cd9f80
nifanfa <nifanfa@foxmail.com>
2022-06-14 02:18:10 +0800

    Update DateTime.cs

--- Corlib/System/DateTime.cs
+++ Corlib/System/DateTime.cs
@@ -1,4 +1,5 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+﻿#if Kernel
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using MOOS;
@@ -1692,3 +1693,4 @@ namespace System
         */
     }
 }
+#endif
