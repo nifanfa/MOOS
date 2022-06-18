e69df3961efc5b86d468555f06ae825d1f2996be
nifanfa <nifanfa@foxmail.com>
2022-06-19 00:09:17 +0800

    Update Threading.cs
    Update Program.cs
    
    Revert "Update Program.cs"
    
    This reverts commit 200bc1e57d2a38c7b341c243dd294f53057d1bdc.
    
    Update Threading
    
    Update Desktop.cs

--- Kernel/Misc/Threading.cs
+++ Kernel/Misc/Threading.cs
@@ -1,3 +1,7 @@
+/*
+ * Copyright(c) 2022 nifanfa, This code is part of the Moos licensed under the MIT licence.
+ */
+
 using Internal.Runtime.CompilerServices;
 using MOOS.Driver;
 using System.Collections.Generic;
@@ -12,7 +16,6 @@ namespace MOOS.Misc
         public IDT.IDTStackGeneric* Stack;
         public ulong SleepingTime;
         public int RunOnWhichCPU;
-        public bool IsIdleThread;
 
         public Thread(delegate*<void> method)
         {
@@ -35,42 +38,24 @@ namespace MOOS.Misc
             SleepingTime = 0;
         }
 
-        public Thread Start(bool is_idle_thread = false) 
+        public Thread Start()
         {
             //Bootstrap CPU
             this.RunOnWhichCPU = 0;
-            this.IsIdleThread = is_idle_thread;
             ThreadPool.Threads.Add(this);
             return this;
         }
 
-        public Thread Start(int run_on_which_cpu, bool is_idle_thread = false)
+        public Thread Start(int run_on_which_cpu)
         {
-            bool hasThatCPU = false;
-            for(int i = 0; i < ACPI.LocalAPIC_CPUIDs.Count; i++) 
-            {
-                if (ACPI.LocalAPIC_CPUIDs[i] == run_on_which_cpu) hasThatCPU = true;
-            }
-            if (!hasThatCPU)
-            {
-                //Didn't find that CPU. run on bootstrap CPU
-                run_on_which_cpu = 0;
-            }
-
             this.RunOnWhichCPU = run_on_which_cpu;
-            this.IsIdleThread = is_idle_thread;
             ThreadPool.Threads.Add(this);
             return this;
         }
 
-        public static void Sleep(ulong Millionseconds) 
+        public static void Sleep(ulong Millionsecos)
         {
-            ThreadPool.Threads[ThreadPool.Index].SleepingTime = Millionseconds;
-        }
-
-        public static void Sleep(int Index,ulong Millionseconds)
-        {
-            ThreadPool.Threads[Index].SleepingTime = Millionseconds;
+            ThreadPool.Threads[ThreadPool.Index].SleepingTime = Millionsecos;
         }
     }
 
@@ -78,18 +63,16 @@ namespace MOOS.Misc
     {
         public static List<Thread> Threads;
         public static bool Initialized = false;
-
-        public static bool Locked;
-        public static volatile uint Locker;
-
+        public static bool Locked = false;
+        public static long Locker = 0;
 
         internal static int Index
         {
-            get 
+            get
             {
                 return Indexs[SMP.ThisCPU];
             }
-            set 
+            set
             {
                 Indexs[SMP.ThisCPU] = value;
             }
@@ -99,30 +82,31 @@ namespace MOOS.Misc
         {
             Native.Cli();
             //Bootstrap CPU
-            if(SMP.ThisCPU == 0)
+            if (SMP.ThisCPU == 0)
             {
                 byte size = 0;
                 for (int i = 0; i < ACPI.LocalAPIC_CPUIDs.Count; i++)
                     if (ACPI.LocalAPIC_CPUIDs[i] > size) size = ACPI.LocalAPIC_CPUIDs[i];
                 Indexs = new int[size + 1];
 
-                UnLock();
-
+                Locked = false;
                 Initialized = false;
                 Threads = new();
-                new Thread(&IdleThread).Start(true);
+                //At least a thread for each CPU to make Thread Pool work
+                new Thread(&IdleThread).Start();
                 Initialized = true;
             }
             //Application CPU
             else
             {
-                new Thread(&IdleThread).Start((int)SMP.ThisCPU, true);
+                //At least a thread for each CPU to make Thread Pool work
+                new Thread(&IdleThread).Start((int)SMP.ThisCPU);
             }
             Native.Sti();
             _int20h(); //start scheduling
         }
 
-        internal static void Terminate()
+        public static void Terminate()
         {
             Console.Write("Thread ");
             Console.Write(Index.ToString());
@@ -133,38 +117,20 @@ namespace MOOS.Misc
         }
 
         [DllImport("*")]
-        private static extern void _int20h();
+        public static extern void _int20h();
 
-        public static void Lock() 
-        {
-            if(!ThreadPool.Locked)
-            {
-                Locker = SMP.ThisCPU;
-                LocalAPIC.SendAllInterrupt(0x20);
-                ThreadPool.Locked = true;
-            }
-        }
-
-        public static void UnLock()
-        {
-            Locker = 0xFFFFFFFF;
-            ThreadPool.Locked = false;
-        }
-
-        public static bool CanLock => Unsafe.As<bool, ulong>(ref ThreadPool.Locked);
-
-        private static void TestThread()
+        public static void TestThread()
         {
             Console.WriteLine("Non-Loop Thread Test!");
             return;
         }
 
-        private static void A()
+        public static void A()
         {
             for (; ; ) Console.WriteLine("Thread A");
         }
 
-        private static void B()
+        public static void B()
         {
             for (; ; ) Console.WriteLine("Thread B");
         }
@@ -174,60 +140,67 @@ namespace MOOS.Misc
             for (; ; ) Native.Hlt();
         }
 
-        private static int[] Indexs;
+        public static int[] Indexs;
 
-        private static ulong TickInSec;
-        private static ulong TickIdle;
-        private static ulong LastSec;
-        public static ulong CPUUsage;
+        public static bool CanLock => Unsafe.As<bool, ulong>(ref Initialized);
 
-        internal static void Schedule(IDT.IDTStackGeneric* stack)
+        public static void Lock() 
         {
-            if (!Initialized  || (Locked && Locker == 0 && SMP.ThisCPU == 0) || Threads.Count == 0) return;
-            while (Locked && Locker != SMP.ThisCPU) Native.Nop();
+            Locker = SMP.ThisCPU;
+            Locked = true;
+            LocalAPIC.SendAllInterrupt(0x20);
+        }
 
-            if (
-                !Threads[Index].Terminated &&
-                Threads[Index].RunOnWhichCPU == SMP.ThisCPU
-                )
+        public static void UnLock() 
+        {
+            Locked = false;
+        }
+
+        public static int ThreadCount => Threads.Count;
+
+        public static void Schedule(IDT.IDTStackGeneric* stack)
+        {
+            if (!Initialized) return;
+
+            if (Locked && Locker != SMP.ThisCPU)
             {
-                Native.Movsb(Threads[Index].Stack, stack, (ulong)sizeof(IDT.IDTStackGeneric));
+                while (Locked) Native.Nop();
+                return;
             }
 
-            for(int i = 0; i < Threads.Count; i++) 
+            if (SMP.ThisCPU == 0)
+            {
+                for (int i = 0; i < Threads.Count; i++)
+                {
+                    if (Threads[i].SleepingTime > 0)
+                    {
+                        Threads[i].SleepingTime--;
+                    }
+                }
+            }
+
+            for(; ; )
             {
                 if (
-                    Threads[i].SleepingTime > 0 &&
-                    Threads[i].RunOnWhichCPU == SMP.ThisCPU
-                    ) 
-                    Threads[i].SleepingTime--;
+                    !Threads[Index].Terminated &&
+                    Threads[Index].RunOnWhichCPU == SMP.ThisCPU
+                    )
+                {
+                    Native.Movsb(Threads[Index].Stack, stack, (ulong)sizeof(IDT.IDTStackGeneric));
+                    break;
+                }
+                Index = (Index + 1) % Threads.Count;
             }
 
             do
             {
                 Index = (Index + 1) % Threads.Count;
-            } while 
+            } while
             (
                 Threads[Index].Terminated ||
                 (Threads[Index].SleepingTime > 0) ||
                 Threads[Index].RunOnWhichCPU != SMP.ThisCPU
             );
-
-            #region CPU Usage
-            if (LastSec != RTC.Second)
-            {
-                if (TickInSec != 0 && TickIdle != 0)
-                    CPUUsage = 100 - ((TickIdle * 100) / TickInSec);
-                TickIdle = 0;
-                TickInSec = 0;
-                LastSec = RTC.Second;
-            }
-            if (Threads[Index].IsIdleThread)
-            {
-                TickIdle++;
-            }
-            TickInSec++;
-            #endregion
 
             Native.Movsb(stack, Threads[Index].Stack, (ulong)sizeof(IDT.IDTStackGeneric));
         }
