bcf78b6a3333493f2d3e70b63d7b5b50fca3055f
nifanfa <nifanfa@foxmail.com>
2022-07-08 20:32:07 +0800

    Update Doom

---
+++ Corlib/System/Windows/Forms/MessageBox.cs
@@ -0,0 +1,18 @@
+﻿#if Kernel && HasGUI
+using MOOS.GUI;
+
+namespace System.Windows.Forms
+{
+    public static class MessageBox
+    {
+        public static void Show(string text)
+        {
+            Desktop.msgbox.X = WindowManager.Windows[0].X + 75;
+            Desktop.msgbox.Y = WindowManager.Windows[0].Y + 75;
+            Desktop.msgbox.SetText(text);
+            WindowManager.MoveToEnd(Desktop.msgbox);
+            Desktop.msgbox.Visible = true;
+        }
+    }
+}
+#endif
