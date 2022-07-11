5d337aa74052fc2fb4f1835c7811badf04dab374
nifanfa <nifanfa@foxmail.com>
2022-07-11 17:38:24 +0800

    Update PS2Controller

---
+++ Kernel/Driver/PS2Controller.cs
@@ -0,0 +1,11 @@
+﻿namespace MOOS.Driver
+{
+    public static class PS2Controller
+    {
+        public static void Initialize()
+        {
+            PS2Keyboard.Initialize();
+            PS2Mouse.Initialise();
+        }
+    }
+}
