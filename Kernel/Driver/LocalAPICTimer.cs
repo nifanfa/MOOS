c4f678a65378e1eb5a654b70580aa18a02f6b4cd
nifanfa <nifanfa@foxmail.com>
2022-07-11 13:29:35 +0800

    Update APIC Timer

--- Kernel/Driver/LocalAPICTimer.cs
+++ Kernel/Driver/LocalAPICTimer.cs
@@ -4,20 +4,20 @@ namespace MOOS.Driver
 {
     public static partial class LocalAPIC
     {
-        public static ulong Ticks => In(0x390);
+        public static ulong Ticks => ReadRegister(0x390);
 
         public static void StartTimer(ulong hz, uint irq)
         {
             hz *= 16;
-            Out(0x320, 0x00020000 | irq);
-            Out(0x3e0, 0x3);
-            Out(0x380, (uint)(Timer.CPU_Clock / hz));
+            WriteRegister(0x320, 0x00020000 | irq);
+            WriteRegister(0x3e0, 0x3);
+            WriteRegister(0x380, (uint)(Timer.CPU_Clock / hz));
             Interrupts.EnableInterrupt(0x20);
         }
 
         public static void StopTimer()
         {
-            Out(0x320, 0x00020000 | 0x10000);
+            WriteRegister(0x320, 0x00020000 | 0x10000);
         }
     }
 }
