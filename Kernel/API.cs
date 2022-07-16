877bc00c58355849dffea476929ae9ddf0186b45
nifanfa <nifanfa@foxmail.com>
2022-07-17 02:01:34 +0800

    Update API

--- Kernel/API.cs
+++ Kernel/API.cs
@@ -1,4 +1,5 @@
 using MOOS.Misc;
+using System.Diagnostics;
 using static IDT;
 using static Internal.Runtime.CompilerHelpers.InteropHelpers;
 
@@ -14,6 +15,10 @@ namespace MOOS
                     return (delegate*<void>)&SayHello;
                 case "Console.WriteLine":
                     return (delegate*<string, void>)&API_WriteLine;
+                case "Allocate":
+                    return (delegate*<ulong, nint>)&API_Allocate;
+                case "Free":
+                    return (delegate*<nint, ulong>)&API_Free;
             }
             Panic.Error($"System call \"{name}\" is not found");
             return null;
@@ -22,6 +27,18 @@ namespace MOOS
         public static void API_WriteLine(string s) 
         {
             Console.WriteLine(s);
+        }
+
+        public static nint API_Allocate(ulong size)
+        {
+            //Debug.WriteLine($"API_Allocate {size}");
+            return Allocator.Allocate(size);
+        }
+
+        public static ulong API_Free(nint ptr)
+        {
+            //Debug.WriteLine($"API_Free 0x{((ulong)ptr).ToString("x2")}");
+            return Allocator.Free(ptr);
         }
 
         public static void SayHello()
