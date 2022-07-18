6ef92bdaa7e5cd0f554c5b4858a4b9cee57b89ea
nifanfa <nifanfa@foxmail.com>
2022-07-18 16:04:05 +0800

    Update Lockscreen.cs

--- Kernel/GUI/Lockscreen.cs
+++ Kernel/GUI/Lockscreen.cs
@@ -111,8 +111,11 @@ namespace MOOS.GUI
             _4.Dispose();
             res.Dispose();
 
+            /*
             string tips = "Press W or Up Arrow to unlock";
             WindowManager.font.DrawString((Framebuffer.Width / 2) - (WindowManager.font.MeasureString(tips) / 2), Framebuffer.Height - (Framebuffer.Height / 6), tips);
+            tips.Dispose();
+            */
         }
     }
 }
