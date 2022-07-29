0c3f9b709866a8b3fde54d234a55048eb97c2dff
nifanfa <nifanfa@foxmail.com>
2022-07-30 00:19:57 +0800

    Update image resampling resize

---
+++ Corlib/System/MathF.cs
@@ -0,0 +1,27 @@
+﻿namespace System
+{
+    public static class MathF
+    {
+        public static double Floor(double x)
+        {
+            if (x >= 0.0)
+            {
+                if (x < ((long.MaxValue / 2 + 1) * 2.0))
+                {
+                    return (double)(long)x;
+                }
+                return x;
+            }
+            else if (x < 0.0)
+            {
+                if (x >= long.MinValue)
+                {
+                    long ix = (long)x;
+                    return (ix == x) ? x : (double)(ix - 1);
+                }
+                return x;
+            }
+            return x;
+        }
+    }
+}
