febca299229c361cb781c7dec31951bb1702fade
nifanfa <nifanfa@foxmail.com>
2022-07-30 01:35:11 +0800

    Update MathF

--- Corlib/System/MathF.cs
+++ Corlib/System/MathF.cs
@@ -1,23 +1,66 @@
-﻿namespace System
+﻿using System.Runtime.InteropServices;
+
+namespace System
 {
-    public static class MathF
+    public static unsafe class MathF
     {
-        public static double Floor(double x)
+        [DllImport("*")]
+        static extern void _fsin(float* x);
+
+        [DllImport("*")]
+        static extern void _fcos(float* x);
+
+        [DllImport("*")]
+        static extern void _fsqrt(float* x);
+
+        [DllImport("*")]
+        static extern void frndint(float* x);
+
+        public static float Sin(float x) 
+        {
+            _fsin(&x);
+            return x;
+        }
+
+        public static float Cos(float x)
+        {
+            _fcos(&x);
+            return x;
+        }
+
+        public static float Tan(float x)
+        {
+            return Sin(x) / Cos(x);
+        }
+
+        public static float Sqrt(float x)
+        {
+            _fsqrt(&x);
+            return x;
+        }
+
+        public static float Round(float x)
+        {
+            frndint(&x);
+            return x;
+        }
+
+        public static float Floor(float x)
         {
             if (x >= 0.0)
             {
-                if (x < ((long.MaxValue / 2 + 1) * 2.0))
+                if (x < ((int.MaxValue / 2 + 1) * 2.0))
                 {
-                    return (double)(long)x;
+                    return (float)(int)x;
                 }
                 return x;
             }
             else if (x < 0.0)
             {
-                if (x >= long.MinValue)
+                if (x >= int.MinValue)
                 {
-                    long ix = (long)x;
-                    return (ix == x) ? x : (double)(ix - 1);
+                    int ix = (int)x;
+                    return (ix == x) ? x : (float)(ix - 1);
                 }
                 return x;
             }
