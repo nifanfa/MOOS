d52b94607f890b28725529d217e1bd9fa0a05625
Elijah629 <EliOzcan@outlook.com>
2022-08-01 16:25:06 -0700

    Features and Fixes

--- Corlib/System/Drawing/Color.cs
+++ Corlib/System/Drawing/Color.cs
@@ -2,6 +2,8 @@ namespace System.Drawing
 {
     public struct Color
     {
+        public byte A, R, G, B;
+
         public static Color AliceBlue { get { return FromArgb(240, 248, 255); } }
         public static Color LightSalmon { get { return FromArgb(255, 160, 122); } }
         public static Color AntiqueWhite { get { return FromArgb(250, 235, 215); } }
@@ -143,60 +145,6 @@ namespace System.Drawing
         public static Color LightPink { get { return FromArgb(255, 182, 193); } }
         public static Color YellowGreen { get { return FromArgb(154, 205, 50); } }
 
-        public uint ARGB;
-
-        public byte A
-        {
-            get
-            {
-                return ((byte)((ARGB >> 24) & 0xFF));
-            }
-            set
-            {
-                ARGB &= ~0xFF000000;
-                ARGB |= (uint)(value << 24);
-            }
-        }
-
-        public byte R
-        {
-            get
-            {
-                return ((byte)((ARGB >> 16) & 0xFF));
-            }
-            set
-            {
-                ARGB &= ~0x00FF0000U;
-                ARGB |= (uint)(value << 16);
-            }
-        }
-
-        public byte G
-        {
-            get
-            {
-                return ((byte)((ARGB >> 8) & 0xFF));
-            }
-            set
-            {
-                ARGB &= ~0x0000FF00U;
-                ARGB |= (uint)(value << 8);
-            }
-        }
-
-        public byte B
-        {
-            get
-            {
-                return ((byte)((ARGB >> 0) & 0xFF));
-            }
-            set
-            {
-                ARGB &= ~0x000000FFU;
-                ARGB |= (uint)(value << 0);
-            }
-        }
-
         public static bool operator ==(Color a, Color b)
         {
             return Equals(a, b);
@@ -209,12 +157,16 @@ namespace System.Drawing
 
         public static bool Equals(Color a, Color b)
         {
-            return a.ARGB == b.ARGB;
+            return
+                a.A == b.A &&
+                a.R == b.R &&
+                a.G == b.G &&
+                a.B == b.B;
         }
 
         public uint ToArgb()
         {
-            return ARGB;
+            return ToArgb(this.A, this.R, this.G, this.B);
         }
 
         public static uint ToArgb(byte r, byte g, byte b)
@@ -229,17 +181,17 @@ namespace System.Drawing
 
         public static Color FromArgb(byte red, byte green, byte blue)
         {
-            return new Color() { ARGB = ToArgb(red,green,blue) };
+            return new Color() { A = 255, R = red, G = green, B = blue };
         }
 
         public static Color FromArgb(byte alpha, byte red, byte green, byte blue)
         {
-            return new Color() { ARGB = ToArgb(alpha,red, green, blue) };
+            return new Color() { A = alpha, R = red, G = green, B = blue };
         }
 
         public static Color FromArgb(uint argb)
         {
-            return new Color() { ARGB = argb };
+            return new Color() { A = (byte)((argb >> 24) & 0xFF), R = (byte)((argb >> 16) & 0xFF), G = (byte)((argb >> 8) & 0xFF), B = (byte)((argb) & 0xFF) };
         }
     }
 }
