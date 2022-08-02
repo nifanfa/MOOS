4374d1520b4d97d2845066fab7bbaa00c2413622
Λv∂lΘи ™ <jaimeraulmendezlopez325@hotmail.com>
2022-08-01 18:57:59 -0700

    Desktop

--- Corlib/System/Char.cs
+++ Corlib/System/Char.cs
@@ -1,31 +1,48 @@
+﻿using System.Globalization;
+
 namespace System
 {
-    public struct Char
-    {
-        public override string ToString()
-        {
-            var r = " ";
-            r._firstChar = this;
+	public struct Char
+	{
+		public override string ToString()
+		{
+			string r = " ";
+			r._firstChar = this;
 
-            return r;
-        }
+			return r;
+		}
 
-        public char ToUpper()
+        public char ToUpper() 
         {
             char chr = this;
             if (chr >= 'a' && chr <= 'z')
                 chr -= (char)('a' - 'A');
             return chr;
         }
-        public char ToLower()
-        {
-            char chr = this;
-            if (chr >= 'A' && chr <= 'Z')
-                chr += (char)('a' - 'A');
-            return chr;
-        }
 
-        public static bool IsDigit(char c)
-            => c >= '0' && c <= '9';
-    }
+		public char ToLower()
+		{
+			return this >= 'A' && this <= 'Z' ? (char)(this + 32) : this;
+		}
+
+		public static bool IsDigit(char c)
+		{
+			return c >= '0' && c <= '9';
+		}
+
+		public static bool IsWhiteSpace(char c)
+		{
+			return IsLatin1(c) ? IsWhiteSpaceLatin1(c) : CharUnicodeInfo.IsWhiteSpace(c);
+		}
+
+		private static bool IsLatin1(char ch)
+		{
+			return ch <= '\x00ff';
+		}
+
+		private static bool IsWhiteSpaceLatin1(char c)
+		{
+			return (c == ' ') || (c >= '\x0009' && c <= '\x000d') || c == '\x00a0' || c == '\x0085';
+		}
+	}
 }
