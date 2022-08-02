0e5cd237f5fec1f5e089de4eb1375a6cce29c80a
nifanfa <nifanfa@foxmail.com>
2022-08-02 08:44:30 +0800

    Revert DrawLine

--- Kernel/Graph/Graphics.cs
+++ Kernel/Graph/Graphics.cs
@@ -293,55 +293,126 @@ namespace MOOS.Graph
 
 		private void DrawLineInternal(Color color, int x0, int y0, int x1, int y1)
 		{
-			int dx = Math.Abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
-			int dy = Math.Abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
-			int err = dx - dy, e2, x2;                       /* error value e_xy */
-			int ed = (int)(dx + dy == 0 ? 1 : Math.Sqrt(((float)dx * dx) + ((float)dy * dy)));
+			DrawLine(x0, y0, x1, y1, color.ToArgb());
 
-			for (; ; ) /* pixel loop */
+			#region Xiaolin Wu's line algorithm
+			// swaps two numbers
+			void Swap(int* a, int* b)
 			{
-				color.A = (byte)(Math.Abs(err - dx + dy) / ed);
-				DrawPoint(color, x0, y0);
-				e2 = err;
-				x2 = x0;
-				if (2 * e2 >= -dx)
-				{
-					/* x step */
-					if (x0 == x1)
-					{
-						break;
-					}
-
-					if (e2 + dy < ed)
-					{
-						color.A = (byte)((e2 + dy) / ed);
-						DrawPoint(color, x0, y0 + sy);
-					}
-
-					err -= dy;
-					x0 += sx;
-				}
-				if (2 * e2 <= dy)
-				{
-					/* y step */
-					if (y0 == y1)
-					{
-						break;
-					}
-
-					if (dx - e2 < ed)
-					{
-						color.A = (byte)((dx - e2) / ed);
-						DrawPoint(color, x2 + sx, y0);
-					}
-
-					err += dx;
-					y0 += sy;
-				}
+				int temp = *a;
+				*a = *b;
+				*b = temp;
 			}
+
+			// returns absolute value of number
+			float Absolute(float x)
+			{
+				if (x < 0) return -x;
+				else return x;
+			}
+
+			//returns integer part of a floating point number
+			int IPartOfNumber(float x)
+			{
+				return (int)x;
+			}
+
+			//rounds off a number
+			int RoundNumber(float x)
+			{
+				return IPartOfNumber(x + 0.5f);
+			}
+
+			//returns fractional part of a number
+			float FPartOfNumber(float x)
+			{
+				if (x > 0) return x - IPartOfNumber(x);
+				else return x - (IPartOfNumber(x) + 1);
+
+			}
+
+			//returns 1 - fractional part of number
+			float RFPartOfNumber(float x)
+			{
+				return 1 - FPartOfNumber(x);
+			}
+
+		// draws a pixel on screen of given brightness
+		// 0<=brightness<=1. We can use your own library
+		// to draw on screen
+		void DrawPoint(int X, int Y, uint Color, float Brightness)
+		{
+			byte A = (byte)((Color >> 24) & 0xFF);
+			byte R = (byte)((Color >> 16) & 0xFF);
+			byte G = (byte)((Color >> 8) & 0xFF);
+			byte B = (byte)((Color) & 0xFF);
+			A = ((byte)(A * (1f - Brightness)));
+				this.DrawPoint(System.Drawing.Color.FromArgb(A,R,G,B), X, Y, true);
 		}
 
-		public void DrawLine(Color color, int x1, int y1, int x2, int y2)
+		void DrawLine(int x0, int y0, int x1, int y1, uint color)
+		{
+			bool steep = Absolute(y1 - y0) > Absolute(x1 - x0);
+
+			// swap the co-ordinates if slope > 1 or we
+			// draw backwards
+			if (steep)
+			{
+				Swap(&x0, &y0);
+				Swap(&x1, &y1);
+			}
+			if (x0 > x1)
+			{
+				Swap(&x0, &x1);
+				Swap(&y0, &y1);
+			}
+
+			//compute the slope
+			float dx = x1 - x0;
+			float dy = y1 - y0;
+			float gradient = dy / dx;
+			if (dx == 0.0)
+				gradient = 1;
+
+			int xpxl1 = x0;
+			int xpxl2 = x1;
+			float intersectY = y0;
+
+			// main loop
+			if (steep)
+			{
+				int x;
+				for (x = xpxl1; x <= xpxl2; x++)
+				{
+					// pixel coverage is determined by fractional
+					// part of y co-ordinate
+					DrawPoint(IPartOfNumber(intersectY), x, color,
+								RFPartOfNumber(intersectY));
+					DrawPoint(IPartOfNumber(intersectY) - 1, x, color,
+								FPartOfNumber(intersectY));
+					intersectY += gradient;
+				}
+			}
+			else
+			{
+				int x;
+				for (x = xpxl1; x <= xpxl2; x++)
+				{
+					// pixel coverage is determined by fractional
+					// part of y co-ordinate
+					DrawPoint(x, IPartOfNumber(intersectY), color,
+								RFPartOfNumber(intersectY));
+					DrawPoint(x, IPartOfNumber(intersectY) - 1, color,
+								  FPartOfNumber(intersectY));
+					intersectY += gradient;
+				}
+			}
+
+		}
+		#endregion
+	}
+
+	public void DrawLine(Color color, int x1, int y1, int x2, int y2)
 		{
 			DrawLineInternal(color, x1, y1, x2, y2);
 		}
