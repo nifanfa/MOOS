9583958ea5f478d5bc7f76bad0f746f644158f40
nifanfa <nifanfa@foxmail.com>
2022-08-05 14:33:16 +0800

    Update List.cs

--- Corlib/System/Collections/Generic/List.cs
+++ Corlib/System/Collections/Generic/List.cs
@@ -1,119 +1,121 @@
 namespace System.Collections.Generic
 {
-	public class List<T>
-	{
-		private T[] _value;
-		private int size;
-		public int Count = 0;
+    public class List<T>
+    {
+        private T[] _value;
 
-		public List(int initsize = 256)
-		{
-			size = initsize;
-			_value = new T[initsize];
-		}
-		public List(T[] array)
-		{
-			size = array.Length;
-			_value = array;
-		}
-		public T this[int index]
-		{
-			get => _value[index];
-			set => _value[index] = value;
-		}
+        public int Count = 0;
 
-		public void Add(T t)
-		{
-			if (size == Count)
-			{
-				Array.Resize(ref _value, size + 1);
-				size++;
-			}
-			_value[Count] = t;
-			Count++;
-		}
+        public List(int initsize = 256)
+        {
+            _value = new T[initsize];
+        }
 
-		public void Insert(int index, T item, bool internalMove = false)
-		{
-			if (!internalMove)
-			{
-				Count++;
-			}
+        public List(T[] t)
+        {
+            _value = t;
+        }
 
-			if (internalMove)
-			{
-				int _index = IndexOf(item);
-				for (int i = _index; i < Count - 1; i++)
-				{
-					_value[i] = _value[i + 1];
-				}
-			}
+        public T this[int index]
+        {
+            get
+            {
+                return _value[index];
+            }
+            set
+            {
+                _value[index] = value;
+            }
+        }
 
-			for (int i = Count - 1; i > index; i--)
-			{
-				_value[i] = _value[i - 1];
-			}
-			_value[index] = item;
-		}
+        public void Add(T t)
+        {
+            if (Count + 1 >= _value.Length) Array.Resize(ref _value, _value.Length + 1);
 
-		public T[] ToArray()
-		{
-			T[] array = new T[Count];
-			for (int i = 0; i < Count; i++)
-			{
-				array[i] = this[i];
-			}
-			return array;
-		}
+            _value[Count] = t;
+            Count++;
+        }
 
-		public int IndexOf(T item)
-		{
-			for (int i = 0; i < Count; i++)
-			{
-				if (this[i] == item)
-				{
-					return i;
-				}
-			}
+        public void Insert(int index, T item, bool internalMove = false)
+        {
+            if (index == IndexOf(item)) return;
 
-			return -1;
-		}
-		public bool Remove(T item)
-		{
-			int at = IndexOf(item);
+            if (!internalMove)
+                Count++;
 
-			if (at < 0)
-			{
-				return false;
-			}
+            if (internalMove)
+            {
+                int _index = IndexOf(item);
+                for (int i = _index; i < Count - 1; i++)
+                {
+                    _value[i] = _value[i + 1];
+                }
+            }
 
-			RemoveAt(at);
+            for (int i = Count - 1; i > index; i--)
+            {
+                _value[i] = _value[i - 1];
+            }
+            _value[index] = item;
+        }
 
-			return true;
-		}
+        public T[] ToArray()
+        {
+            T[] array = new T[Count];
+            for (int i = 0; i < Count; i++)
+            {
+                array[i] = this[i];
+            }
+            return array;
+        }
 
-		public void RemoveAt(int index)
-		{
-			Count--;
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                T first = this[i];
+                T second = item;
 
-			for (int i = index; i < Count; i++)
-			{
-				_value[i] = _value[i + 1];
-			}
+                if (this[i] == item)
+                    return i;
+            }
 
-			_value[Count] = default;
-		}
+            return -1;
+        }
+        public bool Remove(T item)
+        {
+            int at = IndexOf(item);
 
-		public override void Dispose()
-		{
-			_value.Dispose();
-			base.Dispose();
-		}
+            if (at < 0)
+                return false;
 
-		public void Clear()
-		{
-			Count = 0;
-		}
+            RemoveAt(at);
 
-	}
+            return true;
+        }
+
+        public void RemoveAt(int index)
+        {
+            Count--;
+
+            for (int i = index; i < Count; i++)
+            {
+                _value[i] = _value[i + 1];
+            }
+
+            _value[Count] = default(T);
+        }
+
+        public override void Dispose()
+        {
+            _value.Dispose();
+            base.Dispose();
+        }
+
+        public void Clear()
+        {
+            Count = 0;
+        }
+
+    }
 }
