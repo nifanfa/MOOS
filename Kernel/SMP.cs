1320eaf06225ac7f594bfa38d443eff91e762d18
nifanfa <nifanfa@foxmail.com>
2022-08-09 10:56:28 +0800

    Update Trampoline

--- Kernel/SMP.cs
+++ Kernel/SMP.cs
@@ -7,15 +7,19 @@ namespace MOOS
 {
     public static unsafe class SMP
     {
+        public const ulong BaseAddress = 0x50000;
+
         //https://wiki.osdev.org/Memory_Map_(x86)
-        public const ulong APMain = 0x50000;
-        public const ulong Stacks = 0x50008;
-        public const ulong SharedGDT = 0x50016;
-        public const ulong SharedIDT = 0x50024;
-        public const ulong SharedPageTable = 0x51000;
-        public const ulong Trampoline = 0x60000;
+        public const ulong APMain = BaseAddress + 0x0;
+        public const ulong Stacks = BaseAddress + 0x8;
+        public const ulong SharedGDT = BaseAddress + 0x16;
+        public const ulong SharedIDT = BaseAddress + 0x24;
+        public const ulong SharedPageTable = BaseAddress + 0x1000;
+        public const ulong Trampoline = BaseAddress + 0x10000;
 
         public static ulong NumActivedProcessors = 0;
+
+        private const int StackSizeForEachCPU = 1048576;
 
         public static int NumCPU { get => ACPI.LocalAPIC_CPUIDs.Count; }
 
@@ -26,6 +30,7 @@ namespace MOOS
         //GDT, IDT, PageTable has been configured in Trampoline. so we don't need to set it here
         public static void ApplicationProcessorMain(int Core)
         {
+            *(ulong*)Stacks += StackSizeForEachCPU;
             SSE.enable_sse();
             LocalAPIC.Initialize();
             LocalAPICTimer.StartTimer(1000, 0x20);
@@ -44,7 +49,8 @@ namespace MOOS
             *apMain = (ulong)(delegate*<int,void>)&ApplicationProcessorMain;
 
             ulong* stacks = (ulong*)Stacks;
-            *stacks = (ulong)Allocator.Allocate((ulong)(ACPI.LocalAPIC_CPUIDs.Count * 1048576));
+            *stacks = (ulong)Allocator.Allocate((ulong)(ACPI.LocalAPIC_CPUIDs.Count * StackSizeForEachCPU));
+            *stacks += StackSizeForEachCPU;
 
             fixed(GDT.GDTDescriptor* gdt = &GDT.gdtr) 
             {
