362a329bb445280a3570b9957eb608c97c90aa27
nifanfa <nifanfa@foxmail.com>
2022-08-19 06:28:31 +0800

    Update IPAddress

--- Kernel/NET/IPAddress.cs
+++ Kernel/NET/IPAddress.cs
@@ -1,19 +1,20 @@
+using System.Runtime.InteropServices;
+
 namespace System.Net
 {
-    public unsafe class IPAddress
+    [StructLayout(LayoutKind.Sequential,Pack = 1)]
+    public unsafe struct IPAddress
     {
         public uint AddressV4;
 
         public static IPAddress Parse(params byte[] IPAddressV4)
         {
             var IP = new IPAddress();
-            fixed(uint* p = &IP.AddressV4)
-            {
-                ((byte*)p)[0] = IPAddressV4[0];
-                ((byte*)p)[1] = IPAddressV4[1];
-                ((byte*)p)[2] = IPAddressV4[2];
-                ((byte*)p)[3] = IPAddressV4[3];
-            }
+            uint* p = &IP.AddressV4;
+            ((byte*)p)[0] = IPAddressV4[0];
+            ((byte*)p)[1] = IPAddressV4[1];
+            ((byte*)p)[2] = IPAddressV4[2];
+            ((byte*)p)[3] = IPAddressV4[3];
             return IP;
         }
 
